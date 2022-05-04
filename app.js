5c8c129f9e1982a7c879b1b7efa1ef7cc25030fc
youngff43 <99932948+youngff43@users.noreply.github.com>
2022-05-04 18:36:25 -0400

    minor updates

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 const fs = require('fs');
-const generatePage = require('./src/page-template.js');
+const generatePage = require('./src/page-template');
+
 const profileDataArgs = process.argv.slice(2);
+
+console.log(profileDataArgs);
+
 const [theName, github] = profileDataArgs;
 
-fs.writeFile('index.html', generatePage(theName, github), err => {
+console.log(theName, github);
+
+const pageHTML = generatePage(theName, github);
+
+fs.writeFile('./index.html', pageHTML, err => {
   if (err) throw err;
 
   console.log('Portfolio complete! Check out index.html to see the output!');
